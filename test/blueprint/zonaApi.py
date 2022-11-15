d1454a251d8b7fe42de49b5667b16d45f65013ae
Thejairex <Yairjesus777@gmail.com>
2022-11-14 21:59:10 -0300

    Init zonaApi

---
+++ test/blueprint/zonaApi.py
@@ -0,0 +1,5 @@
+# Libraries
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint, jsonify
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, set_access_cookies, unset_jwt_cookies, verify_jwt_in_request
+import hashlib
+
