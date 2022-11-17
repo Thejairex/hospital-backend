b80caa35ec8cfe6ff4ad7dc77a21d9bc52e004f4
Thejairex <Yairjesus777@gmail.com>
2022-11-17 03:33:50 -0300

    fixing zona

--- blueprint/helpers/queryZona.py
+++ blueprint/helpers/queryZona.py
@@ -48,9 +48,9 @@ class qZona:
         try:
             cur = mysql.connection.cursor()
             query = """SELECT z.*, p.dni_paciente, p.nombre "nombre_paciente",p.apellido "apellido_paciente", e.nombre "nombre_enfermero", e.apellido "apellido_enfermero", l.id_llamada FROM zona  z
-				INNER JOIN llamada l on z.id_zona = l.id_zona
-                INNER JOIN enfermero e ON  z.dni_enfermero = e.dni_enfermero
-                INNER JOIN paciente p ON  l.dni_paciente = p.dni_paciente;"""
+				LEFT JOIN llamada l on z.id_zona = l.id_zona
+                LEFT JOIN enfermero e ON  z.dni_enfermero = e.dni_enfermero
+                LEFT JOIN paciente p ON  l.dni_paciente = p.dni_paciente;"""
             if len(column) != 0 and len(data) != 0:
                 query = query + ' WHERE '
                 i = 0
@@ -71,10 +71,10 @@ class qZona:
     def traer_una_zona(self, id_zona):
         try:
             cur = mysql.connection.cursor()
-            query = """SELECT z.*, p.dni_paciente, p.nombre "nombre_paciente",p.apellido "apellido_paciente", e.nombre "nombre_enfermero", e.apellido "apellido_enfermero" FROM zona  z
-				INNER JOIN llamada l on z.id_llamada = l.id_llamada
-                INNER JOIN enfermero e ON  z.dni_enfermero = e.dni_enfermero
-                INNER JOIN paciente p ON  l.dni_paciente = p.dni_paciente WHERE id_zona = {}""".format(id_zona)
+            query = """SELECT z.*, p.dni_paciente, p.nombre "nombre_paciente",p.apellido "apellido_paciente", e.nombre "nombre_enfermero", e.apellido "apellido_enfermero", l.id_llamada FROM zona  z
+				LEFT JOIN llamada l on z.id_zona = l.id_zona
+                LEFT JOIN enfermero e ON  z.dni_enfermero = e.dni_enfermero
+                LEFT JOIN paciente p ON  l.dni_paciente = p.dni_paciente WHERE id_zona = {}""".format(id_zona)
             
             cur.execute(query)
             mysql.connection.commit()
